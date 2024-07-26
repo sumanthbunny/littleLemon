e6ba368b63feb72bc57f245bcc098e320111b214
sumanth vanacharla <vsumanth67@gmail.com>
2024-07-25 23:57:07 -0500

    serializers

---
+++ restaurant/serializers.py
@@ -0,0 +1,12 @@
+from restaurant.models import Booking, Menu
+from rest_framework import serializers
+
+class BookingSerializer(serializers.ModelSerializer):
+     class Meta:
+        model = Booking
+        fields = '__all__'
+
+class MenuSerializer(serializers.ModelSerializer):
+     class Meta:
+        model = Menu
+        fields = '__all__'
